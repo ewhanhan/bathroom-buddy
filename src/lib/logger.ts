87c7c6cf7e965172bd935a9954e125420484b69d
Ewhan Han <40779221+ewhanhan@users.noreply.github.com>
2024-08-03 11:02:10 -0400

    refactor: Refine log comment and add error logger

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,5 +1,9 @@
 import { showLogger } from '@/constant/env'
 
+function formatLogComment(comment?: string): string {
+  return comment ? `${comment}:\n` : ''
+}
+
 /**
  * A logger function that will only logs on development
  * @param object - The object to log
@@ -13,7 +17,25 @@ export function logger(object: unknown, comment?: string): void {
   console.log(
     '%c ============== INFO LOG ============== \n',
     'color: #22D3EE',
-    `${comment ?? ''}${comment ? ':' : ''}\n`,
+    formatLogComment(comment),
+    object,
+  )
+}
+
+/**
+ * An error logger function that will only log errors in development
+ * @param object - The object to log
+ * @param comment - Optional comment to provide context
+ */
+export function errorLogger(object: unknown, comment?: string): void {
+  if (!showLogger) {
+    return
+  }
+
+  console.error(
+    '%c ============== ERROR LOG ============== \n',
+    'color: #EF4444',
+    formatLogComment(comment),
     object,
   )
 }
