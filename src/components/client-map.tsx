54f6641bc7ca1413db8bb26e22bf7e7abee17f72
Ewhan Han <40779221+ewhanhan@users.noreply.github.com>
2024-08-03 11:31:23 -0400

    feat: Load users location to google maps

--- src/components/client-map.tsx
+++ src/components/client-map.tsx
@@ -1,23 +1,40 @@
 'use client'
 
-import React from 'react'
-import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps'
 import { useGeolocation } from '@uidotdev/usehooks'
+import { APIProvider as GoogleMapsProvider, Map, Marker } from '@vis.gl/react-google-maps'
+import React, { useEffect, useState } from 'react'
+import { LoadingSpinner } from './loading-spinner'
 import { logger } from '@/lib/logger'
+import { UNION_STATION } from '@/constant/map'
 
 export function ClientMap() {
-  const position = { lat: 53.54992, lng: 10.00678 }
+  const { loading: isLoadingGeolocation, ...rest } = useGeolocation({ enableHighAccuracy: true, timeout: Infinity })
+  const { latitude, longitude } = rest
+  const [userLocation, setUserLocation] = useState<google.maps.LatLngLiteral>({ lat: latitude ?? UNION_STATION.lat, lng: longitude ?? UNION_STATION.lng })
 
-  const state = useGeolocation()
-  logger(state, 'useGeolocation')
+  useEffect(() => {
+    setUserLocation({
+      lat: latitude ?? UNION_STATION.lat,
+      lng: longitude ?? UNION_STATION.lng,
+    })
+  }, [latitude, longitude])
+
+  if (isLoadingGeolocation) {
+    logger('Loading geolocation...')
+    return (
+      <div className="flex min-h-screen items-center justify-center">
+        <LoadingSpinner size={64} />
+      </div>
+    )
+  }
 
   return (
     <div className="flex size-full flex-col gap-4">
-      <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
-        <Map defaultCenter={position} defaultZoom={10} className="size-full">
-          <Marker position={position} />
+      <GoogleMapsProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
+        <Map defaultCenter={userLocation} defaultZoom={17} className="size-full">
+          <Marker position={userLocation} />
         </Map>
-      </APIProvider>
+      </GoogleMapsProvider>
     </div>
   )
 }
