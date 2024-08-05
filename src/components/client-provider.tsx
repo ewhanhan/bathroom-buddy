ea36e02a9377efa21880c84e443717332eb320e5
Ewhan Han <40779221+ewhanhan@users.noreply.github.com>
2024-08-05 19:45:55 -0400

    feat: Add client provider wrapper

---
+++ src/components/client-provider.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { APIProvider as GoogleMapsProvider } from '@vis.gl/react-google-maps'
+import { ClientMap } from '@/components/client-map'
+
+export function ClientProvider() {
+  return (
+    <GoogleMapsProvider
+      apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}
+      authReferrerPolicy="origin"
+      region="CA"
+      language="en"
+    >
+      <ClientMap />
+    </GoogleMapsProvider>
+  )
+}
